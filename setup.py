114bac92f079612897717596a24b5c749cccbcf8
yihong0618 <zouzou0208@gmail.com>
2023-09-08 19:47:17 +0800

    fix: no lxml

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url="https://github.com/yihong0618/epubhv",
     license="MIT",
     version="0.1.0",
-    install_requires=["bs4", "cssutils"],
+    install_requires=["bs4", "lxml", "cssutils"],
     entry_points={
         "console_scripts": ["epubhv = epubhv.cli:main"],
     },
