4e644341de715125fde86afc9e9d0835d53b8e2a
Jiakuan Li <jiakuan.li.cs@gmail.com>
2023-09-12 00:11:50 -0400

    Integrated with OpenCC (#3)
    * Update requirements.txt to include specific versions of dependencies.
    * Refactor CLI argument parsing and add support for converting EPUB files to specific languages
    * Refactor EPUBHV class, add OpenCC conversion functionality, and enable conversion before packing the EPUB.
    
    ---------
    
    Co-authored-by: yihong0618 <zouzou0208@gmail.com>

--- epubhv/cli.py
+++ epubhv/cli.py
@@ -6,7 +6,7 @@ import os
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument("epub", help="file or dir that contains epub files to change")
     parser.add_argument(
         "--v",
@@ -21,6 +21,46 @@ def main():
         help="change all the epub files to hortical",
     )
 
+    parser.add_argument(
+        "--convert",
+        dest="convert",
+        choices=[
+            "s2t",
+            "t2s",
+            "s2tw",
+            "tw2s",
+            "s2hk",
+            "hk2s",
+            "s2twp",
+            "tw2sp",
+            "t2tw",
+            "hk2t",
+            "t2hk",
+            "t2jp",
+            "jp2t",
+            "tw2t",
+        ],
+        help="""
+change all the epub files to specific language
+
+
+s2t: Simplified Chinese to Traditional Chinese
+t2s: Traditional Chinese to Simplified Chinese
+s2tw: Simplified Chinese to Traditional Chinese (Taiwan Standard)
+tw2s: Traditional Chinese (Taiwan Standard) to Simplified Chinese
+s2hk: Simplified Chinese to Traditional Chinese (Hong Kong variant)
+hk2s: Traditional Chinese (Hong Kong variant) to Simplified Chinese
+s2twp: Simplified Chinese to Traditional Chinese (Taiwan variant)
+tw2sp: Traditional Chinese (Taiwan variant) to Simplified Chinese
+t2tw: Traditional Chinese (OpenCC Standard) to Taiwan Standard
+hk2t: Traditional Chinese (Hong Kong variant) to Traditional Chinese
+t2hk: Traditional Chinese (OpenCC Standard) to Hong Kong variant
+t2jp: Traditional Chinese Characters (Kyūjitai) to New Japanese Kanji
+jp2t: New Japanese Kanji to Traditional Chinese Characters (Kyūjitai)
+tw2t: Traditional Chinese (OpenCC Standard) to Traditional Chinese (Taiwan standard)
+        """,
+    )
+
     options = parser.parse_args()
     epub_files = Path(options.epub)
     # default is to to_vertical
@@ -29,20 +69,19 @@ def main():
         method = "to_horizontal"
     elif options.v:
         method = "to_vertical"
-
     if epub_files.exists():
         if epub_files.is_dir():
             files = list_all_epub_in_dir(epub_files)
             for f in files:
                 print(f"{str(f)} is {method}")
                 try:
-                    e = EPUBHV(f)
+                    e = EPUBHV(f, convert_to=options.convert)
                     e.run(method)
                 except Exception as e:
                     print(f"{str(f)} {method} is failed by {str(e)}")
         else:
             print(f"{str(epub_files)} is {method}")
-            e = EPUBHV(epub_files)
+            e = EPUBHV(epub_files, convert_to=options.convert)
             e.run(method)
     else:
         raise Exception("Please make sure it is a dir contains epub or is a epub file.")
--- epubhv/epubhv.py
+++ epubhv/epubhv.py
@@ -2,14 +2,15 @@
 Follow these steps to change epub books to vertical or horizontal.
 
 """
+import logging
 import os
 import shutil
 import zipfile
 from collections import defaultdict
 from pathlib import Path
-import logging
 
 import cssutils
+import opencc
 from bs4 import BeautifulSoup as bs
 
 cssutils.log.setLevel(logging.CRITICAL)
@@ -53,13 +54,19 @@ def load_opf_meta_data(opf_file):
 
 
 class EPUBHV:
-    def __init__(self, file_name):
+    def __init__(self, file_name, convert_to=None):
         self.epub_file = Path(file_name)
         self.has_css_file = False
         self.files_dict = {}
         self.book_path = None
         self.book_name = None
         self.opf_file = None
+        if convert_to is not None:
+            self.converter = opencc.OpenCC(convert_to)
+            self.convert_to = convert_to
+        else:
+            self.converter = None
+            self.convert_to = None
 
     def extract_one_epub_to_dir(self):
         assert self.epub_file.suffix == ".epub", f"{self.epub_file} Must be epub file"
@@ -187,11 +194,6 @@ html {
             ):
                 self._add_stylesheet_to_html(f, V_STYLE_LINE)
 
-        book_name_v = f"{self.book_name}-v.epub"
-        shutil.make_archive(book_name_v, "zip", self.book_path)
-        os.rename(book_name_v + ".zip", book_name_v)
-        shutil.rmtree(self.book_path)
-
     def change_epub_to_horizontal(self):
         """
         steps:
@@ -234,7 +236,41 @@ html {
                 with open(css, "wb") as f:
                     f.write(css_style)
 
-        book_name_v = f"{self.book_name}-h.epub"
+    def convert(self):
+        if self.converter is None:
+            return
+
+        for html_file in (
+            self.files_dict.get(".html", [])
+            + self.files_dict.get(".xhtml", [])
+            + self.files_dict.get(".htm", [])
+        ):
+            with open(html_file, "r") as f:
+                content = f.read()
+            soup = bs(content, "html.parser")
+
+            html_element = soup.find("html")
+            text_elements = html_element.find_all(string=True)
+
+            for element in text_elements:
+                old_text = element.string
+                if old_text is not None:
+                    new_text = self.converter.convert(old_text)
+                    element.string.replace_with(new_text)
+            html_element.replace_with(html_element)
+
+            with open(html_file, "w") as file:
+                file.write(soup.prettify())
+
+    def pack(self, method="to_vertical"):
+        lang = "original"
+        if self.convert_to is not None:
+            lang = self.convert_to
+        if method == "to_vertical":
+            book_name_v = f"{self.book_name}-v-{lang}.epub"
+        else:
+            book_name_v = f"{self.book_name}-h-{lang}.epub"
+
         shutil.make_archive(book_name_v, "zip", self.book_path)
         os.rename(book_name_v + ".zip", book_name_v)
         shutil.rmtree(self.book_path)
@@ -252,3 +288,6 @@ html {
             self.change_epub_to_horizontal()
         else:
             raise Exception("Only support epub to vertical or horizontal for now")
+
+        self.convert()
+        self.pack(method=method)
